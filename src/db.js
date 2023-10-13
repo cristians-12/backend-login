c2063198782b174c00b0f15528eb6f3f8a69d6ce
Cristian <97061228+cristians-12@users.noreply.github.com>
2023-10-13 13:34:42 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async ()=>{
    try{
-    await mongoose.connect('mongodb+srv://cristians-12:vPZ5zrylQqEattGw@cluster0.ks8gf8z.mongodb.net/');
+    await mongoose.connect('mongodb+srv://cristians-12:cristian12@cluster0.ks8gf8z.mongodb.net/');
     console.log("Conectado correctamente")
    }catch(error){
     console.log(error)
