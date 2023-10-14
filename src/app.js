e63a229da3b6a885dbd193914816d5396469ccad
Cristian <97061228+cristians-12@users.noreply.github.com>
2023-10-14 00:28:30 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin:['http://localhost:5173','https://frontend-login-pi.vercel.app']
+  origin:['http://localhost:5173','https://frontend-login-pi.vercel.app'],
+  credentials:true,
 }
 ));
 app.use(morgan('dev'));
